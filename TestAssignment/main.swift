1f9ee357193d439e9c95d28e877e2484282998af
gorloff <gorloff.slava@gmail.com>
2019-07-13 07:36:41 +0500

    Initial Commit

---
+++ TestAssignment/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestAssignment
+//
+//  Created by Вячеслав Горлов on 13/07/2019.
+//  Copyright © 2019 Вячеслав Горлов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
